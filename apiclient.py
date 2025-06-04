6e34e49e9475e7a223c11555da1e73f9f55e7e6a
btrainor-shalion <btrainor@shalion.com>
2025-06-04 09:27:36 +0200

    feat: add apiclient

---
+++ apiclient.py
@@ -0,0 +1,29 @@
+import random
+import requests
+from urllib.parse import urlencode
+
+import request_url_builder
+import seeds_api_payload_faker
+
+
+def create_seed(partial={}, environment="develop"):
+
+    url = request_url_builder.build_url("seeds", environment)
+    payload = seeds_api_payload_faker.fake_seed(partial, environment)
+    headers = {"accept": "application/json", "Content-Type": "application/json"}
+
+    response = requests.post(url, json=payload, headers=headers)
+
+    return response
+
+
+def get_seed(filters={}, environment="develop"):
+
+    url = request_url_builder.build_url("seeds", environment)
+    url += "?" + urlencode(filters)
+
+    headers = {"accept": "application/json", "Content-Type": "application/json"}
+    
+    response = requests.get(url, headers=headers)
+
+    return random.choice(response.json())
