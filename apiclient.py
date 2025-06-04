4d35f75b8586fad0864e51564e2b2eae5b8f4ad4
btrainor-shalion <btrainor@shalion.com>
2025-06-04 17:25:24 +0200

    feat: add function to add store package seeds

--- apiclient.py
+++ apiclient.py
@@ -56,6 +56,35 @@ def create_store_package(partial={}, environment="develop"):
 
     return response
 
+def add_store_package_seed(store_package_id, environment="develop"):
+
+    store_package_url = request_url_builder.build_url("store-packages", environment, filters={"ids": store_package_id})
+    store_package_response = requests.get(store_package_url)
+    store_package = store_package_response.json()[0]
+
+    store_package_boxes_url = request_url_builder.build_url("store-packages", environment=environment, resource_id=store_package_id, related_resource="boxes")
+
+    store_package_boxes = requests.get(store_package_boxes_url).json()
+    store_package_box = random.choice(store_package_boxes)
+
+    seed = get_seed(filters={"store_ids": store_package["store"]["id"]}, environment=environment)
+    timeframe = get_timeframe(environment=environment)
+
+    box_url = request_url_builder.build_url("boxes", environment, resource_id=store_package_box["id"], related_resource="entries")
+    payload = {
+        "entries": [
+            {
+                "seedId": seed["id"],
+                "timeframeId": timeframe["id"],
+                "isForDiscovery": random.choice([True, False])
+            }
+        ]
+    }
+
+    headers = {"accept": "application/json", "Content-Type": "application/json"}
+    response = requests.post(box_url, json=payload, headers=headers)
+
+    return response
 
 def create_retailer_package(partial={}, environment="develop"):
 
